eb61c0a0a090ac524f67d5d6da6e28929860748b
Mayorstick <111825379+Mayorstick@users.noreply.github.com>
2024-09-27 10:15:25 +0100

    Update Bot.py

--- BOT/IRC_Project/Bot.py
+++ BOT/IRC_Project/Bot.py
@@ -1,20 +1,23 @@
 import socket
 import sys
 
-# method called when needing to receive text from server
-def get_text() -> bytes:
-    text: bytes = client.recv(2040)  # receive the text
-
-    if text.find('PING'.encode()) != -1:  # Check if 'PING' is present
-        client.send(f'PONG {text.split()[1]} \r\n'.encode())  # Send back 'PONG'
-
-    return text
-
+if len(sys.argv) <3:
+        print("invalid argument")
+        exit()
 # Initializing variables
 HOST: str = sys.argv[1]  # Server host provided as command line argument
 PORT: int = int(sys.argv[2])  # Server port provided as command line argument
 ident: str = "SuperBot"  # Bot nickname/identifier
 channel: str = "#hello"  # IRC channel
+activeUser : list=[] #list to keep track of the active users
+
+# method called when needing to receive text from server
+def get_text() -> bytes:
+    text: bytes = client.recv(2040)  # receive the text
+
+    if text.find('PING'.encode()) != -1:  # Check if 'PING' is present
+        client.send(f'PONG {text.split()[1]} \r\n'.encode())  # Send back 'PONG'    
+    return text
 
 # Connecting to the IRC server
 client: socket.socket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
@@ -47,6 +50,23 @@ while True:
     try:
         from_server = get_text()
         print(from_server)  # Display server messages
+
+        #keeping track of active users
+        message=from_server.decode('utf-8', errors='ignore')
+        if "JOIN" in message:
+            user = message.split('!')[0][1:]  
+            if user not in activeUser:
+                activeUser.append(user)
+                print(f"{user} joined. Active users: {activeUser}")
+
+        elif "PART" in message or "QUIT" in message:
+            user = message.split('!')[0][1:]  
+
+            if user in activeUser:  # Check if user exists before removing the users
+                activeUser.remove(user)
+                print(f"{user} left. Active users: {activeUser}")
+          
+
 
     except socket.error as e:  # Error handling
         print(f"Socket error: {e}")
