bad0d43fabb3dbc65f5bd04a94888af035f1e3c7
nataliiaash <131267917+nataliiaash@users.noreply.github.com>
2024-10-09 09:09:38 +0100

    privmsg and commit functions msgHandle.cpp

--- server/msgHandle.cpp
+++ server/msgHandle.cpp
@@ -51,3 +51,80 @@ std::string Server::user(Message message, int ind){
     this->clients[fd]->SetRegistered(true);
     return msgTransform(":Welcome to the Internet Relay Network", client->GetNickname(), 001);
 }
+
+std::string Server::notice(Message message, int ind){
+    int fd = this->fds[ind].fd;
+    Client *client = this->clients[fd];
+    if (message.getArguments().size() != 2) return "Wrong number of parameters"
+    std::string recipient = message.getArguments()[0];
+    if (message.getArguments()[1][0] !=":") return "Too many targets"
+    std::string msgContent = message.getArguments()[1].substr(1);
+    for (int i=3; i<message.getArguments().length(); i++){
+        msgContent+=message.getArguments()[i]+" ";
+    }
+
+    //find recipient in the connected clients
+    auto it = std::find_if(clients.begin(), clients.end(),
+                               [&recipient](const std::pair<int, Client *> &pair) {
+                                   return pair.second->GetNickname() == recipient;
+                               });
+
+    if (it == clients.end()) return "No such nick or chanel"
+    //if recipient is found - send message
+    Client *recipientClient = it->second;
+
+    //deliver a message
+    std::string fullMessage = ":" + client->GetNickname() + " PRIVMSG " + recipient + " :" + msgContent + "\r\n";
+    send(recipientClient->GetFd(), fullMessage.c_str(), fullMessage.length(), 0);
+
+    return ""
+
+
+
+}
+
+
+
+
+std::string Server::privmsg(Message message, int ind) {
+    int fd = this->fds[ind].fd;
+    Client *client = this->clients[fd];
+    //check if client is authorised and registered
+    if (!this->clients[fd]->IsAuth()) return msgTransform(":You are not authorized", client->GetNickname(), 451);
+
+    //check the right number of parameters
+    if (message.getArguments().size() < 2) return msgTransform("PRVIMSG: Not enough parameters", client->GetNickname(), 461);
+    std::string recipient = message.getArguments()[0];
+    if (message.getArguments()[1][0] !=":") return msgTransform("PRVIMSG: Too many targets", client->GetNickname(), 407);
+    std::string msgContent = message.getArguments()[1].substr(1);
+    for (int i=3; i<message.getArguments().length(); i++){
+        msgContent+=message.getArguments()[i]+" ";
+    }
+
+    //check if recipient is valid
+    if (recipient.empty() || msgContent.empty()) return msgTransform(":No recipient or message provided", client->GetNickname(), 411);
+    if (msgContent.empty()) {
+        return msgTransform(":No message provided", client->GetNickname(), 412);
+    }
+
+    //find recipient in the connected clients
+    auto it = std::find_if(clients.begin(), clients.end(),
+                           [&recipient](const std::pair<int, Client*>& pair) {
+                               return pair.second->GetNickname() == recipient;
+                           });
+
+    // if recipient doesn't exist return error
+    if (it == clients.end()) return msgTransform(recipient + " :No such nick or chanel", client->GetNickname(), 401);
+
+    //if recipient is found - send message
+    Client *recipientClient = it->second;
+
+    //check if the recipient is registered to receive a message
+    if (!recipientClient->IsRegistered()) return msgTransform(":Recipient isn't registered", client->GetNickname(), 401);
+
+    //deliver a message
+    std::string fullMessage = ":" + client->GetNickname() + " PRIVMSG " + recipient + " :" + msgContent + "\r\n";
+    send(recipientClient->GetFd(), fullMessage.c_str(), fullMessage.length(), 0);
+
+    return ""
+}
